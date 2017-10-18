3e89b5ca040b0e5274face1e92b9694139c20b46
Edouard <edouard@gomez-vaez.com>
2017-10-18 23:16:47 +0200

    Refactorisation des tests : on sépare chaque appel de la méthode testée dans une fonction de test bien nommée, et on ne garde que les tests qui parlent (bords et boucle).
    (Bonne pratique : pas plus d'un appel de la méthode testée par méthode de test).

--- src/test/java/BerlinClockTest.java
+++ src/test/java/BerlinClockTest.java
@@ -7,12 +7,20 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class BerlinClockTest {
 
     @Test
-    public void test() {
+    public void testLastMinute0() {
         BerlinClock berlinClock = new BerlinClock();
         assertThat(berlinClock.getTime(LocalTime.of(14, 0))[3]).isEqualTo("....");
-        assertThat(berlinClock.getTime(LocalTime.of(14, 1))[3]).isEqualTo("0...");
-        assertThat(berlinClock.getTime(LocalTime.of(14, 4))[3]).isEqualTo("0000");
-        assertThat(berlinClock.getTime(LocalTime.of(14, 5))[3]).isEqualTo("....");
-        assertThat(berlinClock.getTime(LocalTime.of(14, 10))[3]).isEqualTo("....");
+    }
+    
+    @Test
+    public void testLastMinute4() {
+        BerlinClock berlinClock = new BerlinClock();
+        assertThat(berlinClock.getTime(LocalTime.of(17, 4))[3]).isEqualTo("0000");
+    }
+
+    @Test
+    public void testLastMinute5() {
+        BerlinClock berlinClock = new BerlinClock();
+        assertThat(berlinClock.getTime(LocalTime.of(17, 5))[3]).isEqualTo("....");
     }
 }
