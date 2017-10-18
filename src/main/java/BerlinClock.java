0c71d491ef9b78ac57d69f9e8bf9418930df11d3
Edouard <edouard@gomez-vaez.com>
2017-10-18 23:12:31 +0200

    On rajoute en vrac un maximum de tests qu'on pense nécessaires.

--- src/main/java/BerlinClock.java
+++ src/main/java/BerlinClock.java
@@ -5,7 +5,13 @@ public class BerlinClock {
     public String[] getTime(LocalTime time) {
 
         String[] clock  = new String[4];
-        clock[3] = "....";
+
+        StringBuilder minLastsRow = new StringBuilder();
+        for (int i = 0; i < 4; i++) {
+            minLastsRow.append(i <= time.getMinute()%5 -1 ? "0" : ".");
+        }
+
+        clock[3] = minLastsRow.toString();
 
         return clock;
     }
--- src/test/java/BerlinClockTest.java
+++ src/test/java/BerlinClockTest.java
@@ -10,5 +10,9 @@ public class BerlinClockTest {
     public void test() {
         BerlinClock berlinClock = new BerlinClock();
         assertThat(berlinClock.getTime(LocalTime.of(14, 0))[3]).isEqualTo("....");
+        assertThat(berlinClock.getTime(LocalTime.of(14, 1))[3]).isEqualTo("0...");
+        assertThat(berlinClock.getTime(LocalTime.of(14, 4))[3]).isEqualTo("0000");
+        assertThat(berlinClock.getTime(LocalTime.of(14, 5))[3]).isEqualTo("....");
+        assertThat(berlinClock.getTime(LocalTime.of(14, 10))[3]).isEqualTo("....");
     }
 }
