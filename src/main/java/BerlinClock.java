447e73c31d1fd4a5fa25c7e151b8395ca9811822
Edouard <edouard@gomez-vaez.com>
2017-10-18 22:55:11 +0200

    Premier test pour la signature de la méthode

---
+++ src/main/java/BerlinClock.java
@@ -0,0 +1,12 @@
+import java.time.LocalTime;
+
+public class BerlinClock {
+
+    public String[] getTime(LocalTime time) {
+
+        String[] clock  = new String[4];
+        clock[3] = "....";
+
+        return clock;
+    }
+}
--- src/test/java/BerlinClockTest.java
+++ src/test/java/BerlinClockTest.java
@@ -1,10 +1,14 @@
 import org.junit.Test;
+
+import java.time.LocalTime;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class BerlinClockTest {
 
     @Test
     public void test() {
-        assertThat(true).isEqualTo(true);
+        BerlinClock berlinClock = new BerlinClock();
+        assertThat(berlinClock.getTime(LocalTime.of(14, 0))[3]).isEqualTo("....");
     }
 }
