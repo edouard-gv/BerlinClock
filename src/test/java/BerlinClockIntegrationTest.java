c0c8be140a421733d065327261c8876e3ed9710a
Edouard <edouard@gomez-vaez.com>
2017-10-18 23:47:02 +0200

    On rajoute les tests d'intégration. Rappel : dans une méthode de test, on peut avoir plusieurs assert dans un test, mais un seul appel à la méthode testée.

---
+++ src/test/java/BerlinClockIntegrationTest.java
@@ -0,0 +1,42 @@
+import org.junit.Test;
+
+import java.time.LocalTime;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class BerlinClockIntegrationTest {
+
+    @Test
+    public void testMidnight() throws Exception {
+        BerlinClock berlinClock = new BerlinClock();
+        LocalTime time = LocalTime.of(0, 0, 0);
+        assertThat(berlinClock.getTime(time)[0]).isEqualTo("0");
+        assertThat(berlinClock.getTime(time)[1]).isEqualTo("....");
+        assertThat(berlinClock.getTime(time)[2]).isEqualTo("....");
+        assertThat(berlinClock.getTime(time)[3]).isEqualTo("...........");
+        assertThat(berlinClock.getTime(time)[4]).isEqualTo("....");
+    }
+
+    @Test
+    public void testJustBeforeMidnight() throws Exception {
+        BerlinClock berlinClock = new BerlinClock();
+        LocalTime time = LocalTime.of(23, 59, 59);
+        assertThat(berlinClock.getTime(time)[0]).isEqualTo(".");
+        assertThat(berlinClock.getTime(time)[1]).isEqualTo("0000");
+        assertThat(berlinClock.getTime(time)[2]).isEqualTo("000.");
+        assertThat(berlinClock.getTime(time)[3]).isEqualTo("00000000000");
+        assertThat(berlinClock.getTime(time)[4]).isEqualTo("0000");
+    }
+
+    @Test
+    public void testLeatTime() throws Exception {
+        BerlinClock berlinClock = new BerlinClock();
+        LocalTime time = LocalTime.of(13, 37, 37);
+        assertThat(berlinClock.getTime(time)[0]).isEqualTo(".");
+        assertThat(berlinClock.getTime(time)[1]).isEqualTo("00..");
+        assertThat(berlinClock.getTime(time)[2]).isEqualTo("000.");
+        assertThat(berlinClock.getTime(time)[3]).isEqualTo("0000000....");
+        assertThat(berlinClock.getTime(time)[4]).isEqualTo("00..");
+    }
+
+}
