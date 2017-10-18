bbb08d33c85c59831859469a24a5ac2df81631b3
Edouard <edouard@gomez-vaez.com>
2017-10-18 22:45:08 +0200

    Environnement avec tests unitaires fonctionnel.

---
+++ src/test/java/BerlinClockTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class BerlinClockTest {
+
+    @Test
+    public void test() {
+        assertThat(true).isEqualTo(true);
+    }
+}
