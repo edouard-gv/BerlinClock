75a869091ad5ffb5d3e28c0733a90b5e612f0a9b
Edouard <edouard@gomez-vaez.com>
2017-10-18 23:36:05 +0200

    Inline des variables, et l'élégance surgit. Petit battement de coeur.
    Ctrl-Alt-n est ton ami.
    
    (Et on renomme aussi les paramètres de la fonction correctement aussi :-/)

--- src/main/java/BerlinClock.java
+++ src/main/java/BerlinClock.java
@@ -6,23 +6,16 @@ public class BerlinClock {
 
         String[] clock  = new String[4];
 
-        int pos1 = 4;
-        int size1 = time.getMinute() % 5;
-        String s1 = getRow(pos1, size1);
-        clock[3] = s1;
-
-        int pos2 = 11;
-        int size2 = time.getMinute() / 5;
-        String s2 = getRow(pos2, size2);
-        clock[2] = s2;
+        clock[3] = getRow(4, time.getMinute() % 5);
+        clock[2] = getRow(11, time.getMinute() / 5);
 
         return clock;
     }
 
-    private String getRow(int pos, int size) {
+    private String getRow(int size, int pos) {
         StringBuilder minLastsRow = new StringBuilder();
-        for (int i = 0; i < pos; i++) {
-            minLastsRow.append(i <= size -1 ? "0" : ".");
+        for (int i = 0; i < size; i++) {
+            minLastsRow.append(i <= pos -1 ? "0" : ".");
         }
         return minLastsRow.toString();
     }
