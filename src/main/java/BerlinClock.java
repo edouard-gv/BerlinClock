e8b07c6f94c6e5ec95179253f8a4a655d415e879
Edouard <edouard@gomez-vaez.com>
2017-10-22 16:49:30 +0200

    Bon un petit refacto quand même, mettre les valeurs d'affichage 0, ., R... dans des constantes.
    (En regardant par dessus l'épaule du groupe voisin.)
    
    Bonne pratique : ces constantes ne doivent pas être appelées dans les tests unitaires. (Ca reviendrait à appeler le code qu'on teste dans le test).

--- src/main/java/BerlinClock.java
+++ src/main/java/BerlinClock.java
@@ -2,11 +2,15 @@ import java.time.LocalTime;
 
 public class BerlinClock {
 
+    private static final char NO_LIGHT = '.';
+    private static final char YELLOW_LIGHT = '0';
+    private static final char RED_LIGHT = 'R';
+
     public String[] getTime(LocalTime time) {
 
         String[] clock  = new String[5];
 
-        clock[0]= (time.getSecond() % 2 == 0 ? "0" : ".");
+        clock[0]= (time.getSecond() % 2 == 0 ? String.valueOf(YELLOW_LIGHT) : String.valueOf(NO_LIGHT));
 
         clock[1]= getRow(4, time.getHour()/5);
         clock[2]= getRow(4, time.getHour()%5);
@@ -19,7 +23,7 @@ public class BerlinClock {
     private String getRow(int size, int pos) {
         StringBuilder minLastsRow = new StringBuilder();
         for (int i = 0; i < size; i++) {
-            minLastsRow.append(i <= pos -1 ? "0" : ".");
+            minLastsRow.append(i <= pos -1 ? String.valueOf(YELLOW_LIGHT) : String.valueOf(NO_LIGHT));
         }
         return minLastsRow.toString();
     }
@@ -27,7 +31,7 @@ public class BerlinClock {
     private String redify(String row) {
         StringBuilder redRow = new StringBuilder();
         for (int i = 0; i < row.length(); i++) {
-            redRow.append (row.charAt(i) == '0' && i%3 == 2 ? 'R' : row.charAt(i));
+            redRow.append (row.charAt(i) == YELLOW_LIGHT && i%3 == 2 ? RED_LIGHT : row.charAt(i));
         }
         return redRow.toString();
     }
@@ -40,7 +44,7 @@ public class BerlinClock {
 
     //should be private, default package to be unittested
     static int getSwitchPos(String str) {
-        return (str.indexOf('.') >= 0? str.indexOf('.') : str.length() );
+        return (str.indexOf(NO_LIGHT) >= 0? str.indexOf(NO_LIGHT) : str.length() );
     }
 
 }
